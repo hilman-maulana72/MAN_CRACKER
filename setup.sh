dd83fa15784fd054886e8b9c73c615186e62f3f0
hilman-xd72 <69209970+hilman-maulana72@users.noreply.github.com>
2020-11-04 21:51:18 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
+pkg install python -y
+clear 
+pkg install filet
+clear
+pkg install ruby -y
+clear
+gem install lolcat
+clear
+figlet YayanXD | lolcat
+sleep 2
+clear
+figlet YayanXD | lolcat
+sleep 2
+clear
+echo
+echo " author : HILMAN_MAULANA XD 2020 " | lolcat
+echo "github : github.com/hilman-maulana72 " | lolcat
+echo "instag : hilman_maulana2020 " | lolcat
+echo
+sleep 2
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+sleep 2
+clear
+pip2 install requests
+clear
+pip2 install tqdm
+clear
+pip2 install mechanize
+clear
+pip install requests bs4
+clear
+python2 contoh.py
